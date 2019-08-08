96fe6bbc6769e539b98e25e579b4b16eaf39a3e3
Tandi Samples <tandisamples@gmail.com>
2019-08-08 23:06:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  cat_name = ["Spirit", "Magyc", "Billy", "Leo"]
 end
 
 def retrieve_element_from_index(array, index_number)
