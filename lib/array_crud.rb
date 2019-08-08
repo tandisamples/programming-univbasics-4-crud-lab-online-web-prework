3f672ec43357fd4c8ee420d7cddef4a7b2ad2bc1
Tandi Samples <tandisamples@gmail.com>
2019-08-08 23:03:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,8 @@ def add_element_to_end_of_array(array, element)
   cat_name = ["Spirit", "Magyc", "Billy", "Leo"]
   
   cat_name.push("Patrick cat")
+  
+  p
 end
 
 def add_element_to_start_of_array(array, element)
